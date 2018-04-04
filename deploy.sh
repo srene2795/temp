7c8bc97e80d895036d68926c69614ed1e928d94f
Srenevasan Nachiyapan <srene.mn@edgenetworks.in>
2018-04-04 11:51:55 +0530

    initial

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get  install python-pip
+sudo pip install pipenv
+sudo pipenv shell
+sudo easy_install https://s3-us-west-2.amazonaws.com/srene-test/eggfiles/mywebserver-0.0.1.dev0-py2.7.egg
+sudo python -m mywebserver
